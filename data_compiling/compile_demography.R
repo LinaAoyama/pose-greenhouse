eba7b86560b28fa45a448ca76e16b1e4f72309d7
Lina Aoyama <laoyama@uoregon.edu>
2021-07-21 17:23:45 -0700

    load data for roots and adult ramet counts

--- data_compiling/compile_demography.R
+++ data_compiling/compile_demography.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 potID <- read_csv(paste(datpath, "/Greenhouse_potID.csv", sep = "")) #pot info and treatment
 stemcount <- read_csv(paste(datpath, "/Greenhouse_seedling_stem_counts.csv", sep="")) #seedling counts
-rametcount <- read_csv(datpath,"/Greenhouse_adult_ramet_counts.csv", sep="") #adult ramet counts
+rametcount <- read_csv(paste(datpath,"/Greenhouse_adult_ramet_counts.csv", sep="")) #adult ramet counts
 leaftraits <- read_csv(paste(datpath,"/Greenhouse_leaf_traits.csv", sep="")) #seedling fresh and dry leaf weight and area
 biomass <- read_csv(paste(datpath,"/Greenhouse_aboveground_biomass.csv", sep="")) #aboveground BRTE and POSE dry weight
 height <- read_csv(paste(datpath,"/Greenhouse_height.csv", sep="")) #seedling height
-root <- read_csv(paste(datpath,"Greenhouse_roots.csv", sep = "")) #seedling root length, avg diameter, etc.
+root <- read_csv(paste(datpath,"/Greenhouse_roots.csv", sep = "")) #seedling root length, avg diameter, etc.
 #seeds<- read_csv(paste(datpath,"/Greenhouse_POSE_seed_weight.csv", sep=""))
