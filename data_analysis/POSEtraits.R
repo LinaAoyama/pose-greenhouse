43f94259f922ac43179d3b55e4d60428197a2846
Lina Aoyama <laoyama@uoregon.edu>
2021-07-22 16:25:31 -0700

    make new file for exploring trait data

---
+++ data_analysis/POSEtraits.R
@@ -0,0 +1,17 @@
+# Preliminary data exploration
+# Traits of Sandberg bluegrass (POSE) by population and by water availability
+
+# Set pathway first
+
+# Data
+source("data_compiling/compile_demography.R")
+
+# Packages
+library(tidyverse) #data wrangling
+library(ggplot2) #plot
+library(ggpubr) #combine plots
+
+# Prep data
+
+#----------------------------------------------------------#
+# Does POSE’s plasticity of traits change with BRTE competition?
